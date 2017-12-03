ea591107633133c55f5c8a139cd2a23812dcee05
Alana McKenzie <alana.mckenzie@gmail.com>
2017-12-03 15:23:23 -0800

    2017 Day 02: Part 2

--- advent2017/code/dec02.py
+++ advent2017/code/dec02.py
@@ -23,6 +23,33 @@ For example, given the following spreadsheet:
 In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
 
 What is the checksum for the spreadsheet in your puzzle input?
+
+--- Part Two ---
+
+"Great work; looks like we're on the right track after all. Here's a star for your effort."
+However, the program seems a little worried. Can programs be worried?
+
+"Based on what we're seeing,
+it looks like all the User wanted is some information about the evenly divisible values in the spreadsheet.
+Unfortunately, none of us are equipped for that kind of calculation - most of us specialize in bitwise operations."
+
+It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is,
+where the result of the division operation is a whole number. They would like you to find those numbers on each line,
+divide them, and add up each line's result.
+
+For example, given the following spreadsheet:
+
+5 9 2 8
+9 4 7 3
+3 8 6 5
+
+    In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+    In the second row, the two numbers are 9 and 3; the result is 3.
+    In the third row, the result is 2.
+
+In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+What is the sum of each row's result in your puzzle input?
 """
 
 
@@ -36,11 +63,36 @@ def get_max_min_checksum(records):
     """
     return sum([max(row) - min(row) for row in records])
 
-if __name__ == '__main__':
 
+def get_divisible_checksum(records):
+    """Calculate the checksum from the input records,
+    which is the quotient of all divisible values in each row
+    
+    :param list(list(int)) records: row of integers 
+    :return: checksum
+    :rtype: int
+    """
+
+    checksum = 0
+    for row in records:
+        for i, item_i in enumerate(row):
+            # compare each set of numbers only once, and account for i >=j, or i < j
+            for item_j in row[i + 1:]:
+                if item_i % item_j == 0:
+                    checksum += item_i // item_j
+                elif item_j % item_i == 0:
+                    checksum += item_j // item_i
+    return checksum
+
+
+if __name__ == '__main__':
     import utils
+
     input = utils.read_file(r'../inputs/dec02_input.txt')
     formatted_input = [[int(item) for item in line.split()] for line in input]
 
     max_min_checksum = get_max_min_checksum(formatted_input)
     print(f'The total max-min checksum is: {max_min_checksum}')
+
+    divisible_checksum = get_divisible_checksum(formatted_input)
+    print(f'The total divisible checksum is: {divisible_checksum}')
--- advent2017/tests/test_dec02.py
+++ advent2017/tests/test_dec02.py
@@ -4,3 +4,8 @@ from advent2017.code import dec02
 def test_max_min_checksum():
     records = [[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]
     assert dec02.get_max_min_checksum(records) == 18
+
+
+def test_divisible_checksum():
+    records = [[5, 9, 2, 8], [9, 4, 7, 3], [3, 8, 6, 5]]
+    assert dec02.get_divisible_checksum(records) == 9
